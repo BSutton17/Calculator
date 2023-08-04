d4e28ce86d72066a077bc7f1ed6786cccfd48e9f
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2023-08-04 15:02:47 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,126 @@
-import { useState } from 'react'
-import './App.css'
-import ReactMarkdown from "react-markdown"
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import Display from "./assets/Display";
 
-const defaultMarkdownText = `
-# Heading 1
-
-## Sub Heading 1
-
-This is a paragraph with some **bolded text**.
-
-### Sub Heading 2
-
-Here's a [link to OpenAI](https://openai.com/).
-
-#### Sub Heading 3
-
-Inline code: \`print("Hello, World!")\`
-
-##### Sub Heading 4
-
-Code block:
-    def factorial(n):
-    if n == 0:
-    return 1
-    else:
-    return n * factorial(n-1)
-
-    List:
-    - Item 1
-    - Item 2
-    - Item 3
-    
-    > Blockquote: The best way to predict the future is to invent it. - Alan Kay
-
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
-
-![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
-
-- And of course there are lists.
-  - Some are bulleted.
-      - With different indentation levels.
-        - That look like this.
-
-That's it!
-
-`
-function App() {
-  const [text, setText] = useState<string>(defaultMarkdownText)
-  return (
-    <>
-      <div>
-        <h1 id="title">Markdown Code Editor</h1>
-        <textarea value={text} onChange={(e) => setText(e.target.value)}placeholder="Enter Markdown Text Here..." id="editor">
-        
-        </textarea>
-        <div id="preview">
-          <ReactMarkdown>{text}</ReactMarkdown>
-        </div>
-        <footer>Created by Bryson Sutton</footer>
-      </div>
-    </>
-  )
+interface Drum {
+  key: string;
+  url: string;
+  name: string;
 }
 
-export default App
+// array of all audio types, their keys and their names
+const drums: Drum[] = [
+  {
+    key: "Q",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    name: "Heater 1",
+  },
+  {
+    key: "W",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    name: "Heater 2",
+  },
+  {
+    key: "E",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    name: "Heater 3",
+  },
+  {
+    key: "A",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    name: "Heater 4",
+  },
+  {
+    key: "S",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    name: "Clap",
+  },
+  {
+    key: "D",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    name: "Open-HH",
+  },
+  {
+    key: "Z",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    name: "Kick-n'-Hat",
+  },
+  {
+    key: "X",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    name: "Kick",
+  },
+  {
+    key: "C",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    name: "Closed-HH",
+  },
+];
+function App() {
+  // state to hold the volume level (default to 0.5)
+  const [volume, setVolume] = useState(0.5);
+
+  // make it so the audio can play by pressing the key on the keyboard
+  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    // locate when a key is being pressed
+    const clip = drums.find((clip) => clip.key === e.key.toUpperCase());
+    if (!clip) return;
+
+    // Get the corresponding audio element
+    const audioElement = document.getElementById(clip.key) as HTMLAudioElement;
+
+    // Set the volume of the audio element
+    audioElement.volume = volume;
+
+    // Play the audio
+    audioElement
+      .play()
+      .then(() => {
+        // Success: Handle the audio play logic here (if needed)
+      })
+      .catch((error) => {
+        console.error("Error playing audio:", error);
+      });
+
+    // make a border around the selected key
+    document.getElementById("drum-" + clip.key)?.focus();
+    // display the name of the key that was pressed
+    document.getElementById("display")!.innerText = clip.name;
+  };
+
+  // handle volume change
+  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newVolume = parseFloat(e.target.value);
+    setVolume(newVolume);
+  };
+
+  return (
+    // HTML
+    <div id="outside" onKeyDown={playAudio}>
+      <h1>Drum Machine</h1>
+      <div id="drum-machine">
+        <div id="pads">
+          {drums.map((clip) => (
+            <Display audio={clip} volume={volume} key={clip.key} />
+          ))}
+        </div>
+        <div id="display" className="fade-out"></div>
+        <div id="volume-control">
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+          <p>Volume: {Math.round(volume * 100)}%</p>
+        </div>
+      </div>
+      <footer>Created by Bryson Sutton</footer>
+    </div>
+  );
+}
+
+export default App;

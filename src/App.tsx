0fd55d1a176273786ccb41e5dceba471a5162669
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2023-08-05 01:02:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,225 @@
 import { useState } from "react";
-import React from "react";
 import "./App.css";
-import Display from "./assets/Display";
 
-interface Drum {
-  key: string;
-  url: string;
-  name: string;
-}
-
-// array of all audio types, their keys and their names
-const drums: Drum[] = [
-  {
-    key: "Q",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    name: "Heater 1",
-  },
-  {
-    key: "W",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    name: "Heater 2",
-  },
-  {
-    key: "E",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    name: "Heater 3",
-  },
-  {
-    key: "A",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-    name: "Heater 4",
-  },
-  {
-    key: "S",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-    name: "Clap",
-  },
-  {
-    key: "D",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-    name: "Open-HH",
-  },
-  {
-    key: "Z",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-    name: "Kick-n'-Hat",
-  },
-  {
-    key: "X",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-    name: "Kick",
-  },
-  {
-    key: "C",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-    name: "Closed-HH",
-  },
-];
 function App() {
-  // state to hold the volume level (default to 0.5)
-  const [volume, setVolume] = useState(0.5);
+  const [answer, setAnswer] = useState("");
+  const [expression, setExpression] = useState("");
+  const et = expression.trim();
 
-  // make it so the audio can play by pressing the key on the keyboard
-  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    // locate when a key is being pressed
-    const clip = drums.find((clip) => clip.key === e.key.toUpperCase());
-    if (!clip) return;
-
-    // Get the corresponding audio element
-    const audioElement = document.getElementById(clip.key) as HTMLAudioElement;
-
-    // Set the volume of the audio element
-    audioElement.volume = volume;
-
-    // Play the audio
-    audioElement
-      .play()
-      .then(() => {
-        // Success: Handle the audio play logic here (if needed)
-      })
-      .catch((error) => {
-        console.error("Error playing audio:", error);
-      });
-
-    // make a border around the selected key
-    document.getElementById("drum-" + clip.key)?.focus();
-    // display the name of the key that was pressed
-    document.getElementById("display")!.innerText = clip.name;
+  const isOperator = (symbol: string) => {
+    return /[*/+-]/.test(symbol);
   };
 
-  // handle volume change
-  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newVolume = parseFloat(e.target.value);
-    setVolume(newVolume);
+  const buttonPress = (symbol: string) => {
+    if (symbol === "clear") {
+      setAnswer("");
+      setExpression("0");
+    } else if (symbol === "negative") {
+      if (answer === "") return;
+      setAnswer(
+        answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+      );
+    } else if (symbol === "percent") {
+      if (answer === "") return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)) {
+      setExpression(et + " " + symbol + " ");
+    } else if (symbol === "=") {
+      calculate();
+    } else if (symbol === "0") {
+      if (expression.charAt(0) !== "0") {
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === ".") {
+      // split by operators and get last number
+      const lastNumber = expression.split(/[-+/*]/g).pop();
+      if (!lastNumber) return;
+      console.log("lastNumber :>> ", lastNumber);
+      // if last number already has a decimal, don't add another
+      if (lastNumber?.includes(".")) return;
+      setExpression(expression + symbol);
+    } else {
+      if (expression.charAt(0) === "0") {
+        setExpression(expression.slice(1) + symbol);
+      } else {
+        setExpression(expression + symbol);
+      }
+    }
+  };
+
+  const calculate = () => {
+    // if last char is an operator, do nothing
+    if (isOperator(et.charAt(et.length - 1))) return;
+    // clean the expression so that two operators in a row uses the last operator
+    // 5 * - + 5 = 10
+    const parts = et.split(" ");
+    const newParts = [];
+
+    // go through parts backwards
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
+        newParts.unshift(parts[i]);
+        let j = 0;
+        let k = i - 1;
+        while (isOperator(parts[k])) {
+          k--;
+          j++;
+        }
+        i -= j;
+      } else {
+        newParts.unshift(parts[i]);
+      }
+    }
+    const newExpression = newParts.join(" ");
+    if (isOperator(newExpression.charAt(0))) {
+      setAnswer(eval(answer + newExpression) as string);
+    } else {
+      setAnswer(eval(newExpression) as string);
+    }
+    setExpression("");
   };
 
   return (
-    // HTML
-    <div id="outside" onKeyDown={playAudio}>
-      <h1>Drum Machine</h1>
-      <div id="drum-machine">
-        <div id="pads">
-          {drums.map((clip) => (
-            <Display audio={clip} volume={volume} key={clip.key} />
-          ))}
-        </div>
-        <div id="display" className="fade-out"></div>
-        <div id="volume-control">
-          <input
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
-          />
-          <p>Volume: {Math.round(volume * 100)}%</p>
+    <>
+      <div className="container">
+        <h1>Calculator Application</h1>
+        <div id="calculator">
+          <div id="display" style={{ textAlign: "right" }}>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
+          </div>
+          <button
+            id="clear"
+            onClick={() => buttonPress("clear")}
+            className="light-gray"
+          >
+            C
+          </button>
+          <button
+            id="negative"
+            onClick={() => buttonPress("negative")}
+            className="light-gray"
+          >
+            +/-
+          </button>
+          <button
+            id="percentage"
+            onClick={() => buttonPress("percentage")}
+            className="light-gray"
+          >
+            %
+          </button>
+          <button
+            id="divide"
+            onClick={() => buttonPress("/")}
+            className="yellow"
+          >
+            /
+          </button>
+          <button
+            id="seven"
+            onClick={() => buttonPress("7")}
+            className="dark-gray"
+          >
+            7
+          </button>
+          <button
+            id="eight"
+            onClick={() => buttonPress("8")}
+            className="dark-gray"
+          >
+            8
+          </button>
+          <button
+            id="nine"
+            onClick={() => buttonPress("9")}
+            className="dark-gray"
+          >
+            9
+          </button>
+          <button
+            id="multiply"
+            onClick={() => buttonPress("*")}
+            className="yellow"
+          >
+            *
+          </button>
+          <button
+            id="four"
+            onClick={() => buttonPress("4")}
+            className="dark-gray"
+          >
+            4
+          </button>
+          <button
+            id="five"
+            onClick={() => buttonPress("5")}
+            className="dark-gray"
+          >
+            5
+          </button>
+          <button
+            id="six"
+            onClick={() => buttonPress("6")}
+            className="dark-gray"
+          >
+            6
+          </button>
+          <button
+            id="subtract"
+            onClick={() => buttonPress("-")}
+            className="yellow"
+          >
+            -
+          </button>
+          <button
+            id="one"
+            onClick={() => buttonPress("1")}
+            className="dark-gray"
+          >
+            1
+          </button>
+          <button
+            id="two"
+            onClick={() => buttonPress("2")}
+            className="dark-gray"
+          >
+            2
+          </button>
+          <button
+            id="three"
+            onClick={() => buttonPress("3")}
+            className="dark-gray"
+          >
+            3
+          </button>
+          <button id="add" onClick={() => buttonPress("+")} className="yellow">
+            +
+          </button>
+          <button
+            id="zero"
+            onClick={() => buttonPress("0")}
+            className="dark-gray"
+          >
+            0
+          </button>
+          <button
+            id="decimal"
+            onClick={() => buttonPress(".")}
+            className="dark-gray"
+          >
+            .
+          </button>
+          <button
+            id="equals"
+            onClick={() => buttonPress("=")}
+            className="yellow"
+          >
+            =
+          </button>
         </div>
       </div>
-      <footer>Created by Bryson Sutton</footer>
-    </div>
+    </>
   );
 }
 
